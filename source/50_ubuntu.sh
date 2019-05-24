85311d4bf6e6df2fa3af9ade763d798be5a88bc5
Sergey Eliseev <gafnium@yandex-team.ru>
2019-05-24 18:20:53 +0300

    some handy settings

--- source/50_ubuntu.sh
+++ source/50_ubuntu.sh
@@ -6,8 +6,14 @@ alias update="sudo apt-get -qq update && sudo apt-get upgrade"
 alias install="sudo apt-get install"
 alias remove="sudo apt-get remove"
 alias search="apt-cache search"
+alias screen="screen -R"
 
 alias say=spd-say
+
+if [ -d $HOME/android-sdk ]; then
+  export ANDROID_SDK_ROOT=$HOME/android-sdk
+  export PATH=$PATH:$ANDROID_SDK_ROOT/tools
+fi
 
 # Make 'less' more.
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
