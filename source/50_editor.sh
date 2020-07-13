b070a50b8fa9c93e3f9752b5cbc977f7b0bcd2c3
Sergey Eliseev <gafnium@yandex-team.ru>
2020-07-13 11:07:58 +0300

    Fix tools; remove graphics editor selector

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -3,16 +3,3 @@
 export EDITOR=vim
 export VISUAL="$EDITOR"
 
-function q() {
-  if [[ -t 0 ]]; then
-    $EDITOR "$@"
-  else
-    # Read from STDIN (and hide the annoying "Reading from stdin..." message)
-    $EDITOR - > /dev/null
-  fi
-}
-alias qv="q $DOTFILES/link/.{,g}vimrc +'cd $DOTFILES'"
-alias qs="q $DOTFILES"
-
-# For when you have vim on the brain
-alias :q=exit
